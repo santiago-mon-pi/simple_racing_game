5b5b00b93e80bcd57370f7977ca747844d1d156e
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-06-27 19:30:59 -0500

    updated code with background color, window title and window size

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import pygame
 from pygame.locals import *
 
+size = width, height (800, 800)
+
 pygame.init()
 running = True
-pygame.display.set_mode((800, 800))
+# set window size
+screen = pygame.display.set_mode((800, 800))
+# sets title
+pygame.display.set_caption("Monpi's race game")
+#set background color
+screen.fill((((48, 25, 52))))
+
+# applies changes
+pygame.display.update()
 
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-
 
 pygame.quit()
